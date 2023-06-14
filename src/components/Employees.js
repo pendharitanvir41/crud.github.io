2d3078a728c55e22b9b1760419597f38a6247a9e
Tanvir Pendhari <pendharitanvir007@gmail.com>
2023-06-14 17:24:41 +0530

    My Crud Application

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+  {
+    id:'1',
+    Name: "Tanvir Pendhari",
+    Age:"22"
+  },
+  {
+    id:'2',
+    Name: "Shreyash Parshetti",
+    Age:"23"
+  },
+  {
+    id:'3',
+    Name: "Rushikesh Gurav",
+    Age:"20"
+  }
+]
+export default Employees;
